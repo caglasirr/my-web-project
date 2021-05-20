1f8a0b823a0871885f1c45d3f85d37d92068beb6
caglasirr <81507686+caglasirr@users.noreply.github.com>
2021-05-20 22:04:12 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,20 @@ import {PricesSectionComponent} from './prices-section/prices-section.component'
 import {HomeComponent} from './home/home.component';
 import {ScreensComponent} from './screens/screens.component';
 import {FooterComponent} from './footer/footer.component';
+import {AboutComponent} from './about/about.component'
+import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 const routes: Routes = [
-  {path:'ozellikler', component: FeaturesSectionComponent},
+  
+  {path:'iletisim', component:ContactFormComponent},
+  {path:'home', component:HomeComponent},
   {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'ozellikler', component: FeaturesSectionComponent},
   {path: 'fiyatlar', component: PricesSectionComponent},
   {path: 'ekranlar', component: ScreensComponent},
-  {path: 'hakkinda', component: FooterComponent},
-  {path:'home', component:HomeComponent}
+  {path: 'hakkinda', component: AboutComponent},
+  
 
 ];
 
